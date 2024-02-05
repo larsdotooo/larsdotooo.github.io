4db565823075148d38503b2a7b37c32c1eb5bab6
larsdotooo <158203517+larsdotooo@users.noreply.github.com>
2024-02-05 09:29:29 +0100

    Add files via upload

--- src/components/Home/RecentPosts.js
+++ src/components/Home/RecentPosts.js
@@ -1,6 +1,6 @@
 import { sortBlogs } from '@/src/utils';
 import Link from 'next/link';
-import React from 'react'
+import React from 'react';
 import BlogLayoutThree from '@/src/components/Blog/BlogLayoutThree';
 
 const RecentPosts = ({blogs}) => {
@@ -11,14 +11,15 @@ const RecentPosts = ({blogs}) => {
         <h2 className='inline-block font-bold capitalize text-4xl'>
         Mehr Gedanken
         </h2>
-        <Link href='/categories/all' className='inline-block font-medium text-orange-500 underline underline-offset-2
-        text-lg'>alle Gedanken</Link>
+        <Link href='/categories/all' className='inline-block font-medium text-orange-500 underline underline-offset-2 text-lg'>
+        alle Gedanken
+        </Link>
         </div>
 
         <div className='grid grid-cols-3 grid-rows-3 gap-16 mt-16'>
             {
-                sortedBlogs.slice(5, 11).map((blog) => {
-                    return <article className='col-span-1 row-span-1 relative'>
+                sortedBlogs.slice(5, 11).map((blog, index) => {
+                    return <article key={index} className='col-span-1 row-span-1 relative'>
                         <BlogLayoutThree blog={blog} />
                     </article>
                 })
